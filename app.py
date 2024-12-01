0947c2c25707301ad27eb17770ca510b469e4219
atharvasalunke <atharvasalunke14@gmail.com>
2024-11-30 23:42:10 -0500

    updated CORS

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from ultralytics import YOLO
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=['http://localhost:4200'])
 
 
 # Directories for storing uploaded files
